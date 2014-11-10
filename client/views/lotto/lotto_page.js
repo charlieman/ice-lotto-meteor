8ec364d857f20b966b89f6f8d82e7f276323dc11
Carlos Zúñiga <carlos.zun@gmail.com>
2014-11-09 22:39:57 -0500

    Arrows and cursor for showing entries to a tier in the Lotto page

--- client/views/lotto/lotto_page.js
+++ client/views/lotto/lotto_page.js
@@ -1,18 +1,21 @@
+var currentTier = function () {
+  var tier = Session.get('SelectedTier');
+  return tier === this.tier;
+};
+
 Template.lottoPage.helpers({
-  currentTier: function() {
-    var tier = Session.get('SelectedTier');
-    return tier === this.tier;
-  },
+  currentTier: currentTier,
   showEntries: function (entries) {
     var tier = Session.get('SelectedTier');
     return {entries: _.filter(entries, function(x) {return x.amount === tier})};
   },
-  midTier: function() {
+  midTier: function () {
     return this.tier === 10;
   }
 });
 
 Template.tierRow.helpers({
+  currentTier: currentTier,
   prizeLoop: function (prizes) {
     var prizeList = [];
     for (var i = 0; i < 10; i++) {
@@ -23,14 +26,16 @@ Template.tierRow.helpers({
 });
 
 Template.tierRow.events({
-  'click tr': function (e) {
+  'click .tier-number': function (e) {
     e.preventDefault();
     var tier = Session.get('SelectedTier');
     if (tier !== this.tier)
       Session.set('SelectedTier', this.tier);
     else
       Session.set('SelectedTier', null);
+  },
+  'click .prize': function (e) {
+    e.preventDefault();
+    Session.set('PrizeSelector', this.prize);
   }
 });
-
-
