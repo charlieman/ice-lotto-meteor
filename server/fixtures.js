1b6b168fe433faa915e55c8ed9f6574ea4b5b588
Carlos Zúñiga <carlos.zun@gmail.com>
2014-11-09 22:33:27 -0500

    Fixed fixtures

--- server/fixtures.js
+++ server/fixtures.js
@@ -14,36 +14,54 @@ if (Lottos.find().count() === 0) {
     password: 'baz',
     profile: {verified: false, alts: []}
   });
+  Accounts.createUser({
+    username: 'borg',
+    password: 'borg',
+    profile: {verified: false, alts: []}
+  });
   var nerium = Meteor.users.findOne({username: 'nerium'});
   var foobar = Meteor.users.findOne({username: 'foobar'});
   var baz = Meteor.users.findOne({username: 'baz'});
+  var borg = Meteor.users.findOne({username: 'borg'});
 
   var tiers = [];
   for (var i = 0; i < 20; i++) {
     var prizes = {};
     for (var j = 0; j < 10; j++) {
       if (Math.random() > 0.3) {
-        prizes[j + 1] = 'prize';
+        prizes[j + 1] = 'prize ' + i + '-' + j;
       }
     }
     tiers.push({tier: i + 1, prizes: prizes});
   }
   var entries = [];
+
+  // for the small pot
+  for (i = 0; i < 5; i++) {
+    if (Math.random() > 0.6) {
+      entries.push({_id: Random.id(), userId: borg._id, username: borg.username, amount: i});
+    }
+  }
   for (i = 0; i < 20; i++) {
     _.each([nerium, foobar, baz], function(user){
-      if (Math.random() > 0.4) {
-        entries.push({_id: Random.id(), userId: user._id, amount: i});
+      if (Math.random() > 0.5) {
+        entries.push({_id: Random.id(), userId: user._id, username: user.username, amount: i});
       }
     });
   }
+
+  var pots = updatePots(entries);
+
   Lottos.insert({
     userId: nerium._id,
-    date: new Date(2014, 11, 1),
+    date: new Date(2014, 10, 1), // month is zero based
     tiers: tiers,
-    smallpot: 20,
-    bigpot: 180,
+    smallpot: pots.smallpot_total,
+    bigpot: pots.bigpot_total,
+    smallpot_entries: pots.smallpot,
+    bigpot_entries: pots.bigpot,
     entries: entries,
-    winners: {},
+    winners: [],
     smallpot_winner: undefined,
     bigpot_winner: undefined,
     created: new Date()
