13a44ed76609a5f97ceb1cea1cffeccf54b00e9f
Carlos Zúñiga <carlos.zun@gmail.com>
2014-11-11 09:48:14 -0500

    Changed the currentTier helper into a general helper for session values

--- client/helpers/spacebars.js
+++ client/helpers/spacebars.js
@@ -1,5 +1,10 @@
-UI.registerHelper('range', function (start, stop, step) {
+Template.registerHelper('range', function (start, stop, step) {
   return _.map(_.range(start, stop, step), function (x) {
     return {value: x};
   })
+});
+
+Template.registerHelper('inSession', function(name, value){
+  var sessionValue = Session.get(name);
+  return sessionValue === value;
 });
