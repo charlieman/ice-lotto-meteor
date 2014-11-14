772696dae6a53aeec9e180aa37521f5931426100
Carlos Zúñiga <carlos.zun@gmail.com>
2014-11-13 21:37:30 -0500

    whitespace

--- server/publications.js
+++ server/publications.js
@@ -1,22 +1,22 @@
-Meteor.publish('lottos', function() {
-  if(!this.userId)
+Meteor.publish('lottos', function () {
+  if (!this.userId)
     return this.ready();
-  return Lottos.find({}, {fields: {date: 1}, limit: 10});
+  return Lottos.find({}, {fields: {date: 1}, limit: 10, sort: {date: -1}});
 });
 
 Meteor.publish('singleLotto', function (id) {
   check(id, String);
-  if(!this.userId)
+  if (!this.userId)
     return this.ready();
   return [
     Lottos.find(id),
-    Tiers.find({lottoId: id})
+    Tiers.find({lottoId: id}, {sort: {tier: 1}})
   ];
 });
 
-Meteor.publish('usernames', function(){
+Meteor.publish('usernames', function () {
   //check(this.userId, String);
-  if(!this.userId)
+  if (!this.userId)
     return this.ready();
   return Meteor.users.find({}, {fields: {username: 1}});
 });
