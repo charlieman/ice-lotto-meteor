5fee453ca5abd12d9eb87a65940fddd4b5f4b1b4
Carlos Zuñiga <carlos.zun@gmail.com>
2016-05-19 01:01:36 -0500

    Added the server-side functionality for the delete lotto button

--- client/views/lotto/lotto_item.js
+++ client/views/lotto/lotto_item.js
@@ -3,3 +3,17 @@ Template.lottoItem.helpers({
     return moment(this.date).format('MMM Do, YYYY');
   }
 });
+
+Template.lottoItem.events({
+  'click .delete': function(e) {
+    e.preventDefault();
+    if (!confirm("Are you sure you want to delete this lotto?")) return;
+
+    Meteor.call('deleteLotto', this._id, function(error, result) {
+      if (error) {
+        return throwError(error.reason);
+      }
+    });
+
+  }
+});
