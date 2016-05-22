24ab480cd990c09d05a93326abdbb44ae3101e04
Carlos Zuñiga <carlos.zun@gmail.com>
2016-05-22 01:06:29 -0500

    Moved pot template to it's own files

---
+++ client/views/lotto/pot.js
@@ -0,0 +1,62 @@
+Template.pot.helpers({
+  potEntries: function(entries, winner) {
+    var rangeStart = 1;
+    var rangeEnd = 0;
+    return _.map(entries, function (v, k) {
+      var _rangeStart = rangeStart;
+      rangeEnd = _rangeStart + v - 1;
+      rangeStart = rangeEnd + 1;
+      return {
+        gwuserId: k,
+        amount: v,
+        rangeStart: _rangeStart,
+        rangeEnd: rangeEnd,
+        winner: k === winner
+      };
+    });
+  },
+  mainUsername: function (gwuserId) {
+    return GWUsers.findOne(gwuserId).alts[0];
+  },
+  half: function(total) {
+    return total / 2;
+  },
+  hasWinner: function() {
+    return !!this.winner;
+  },
+  isLottoOpen: function() {
+    return !Template.parentData().lotto.closed;
+  },
+  attributes: function() {
+    if (!!this.winner) {
+      return { class: "winner" };
+    }
+    return {};
+  }
+});
+
+Template.pot.events({
+  'click .roll': function(e) {
+    e.preventDefault();
+    var lottoId = Session.get('lottoId');
+    var pot = this.toggle;
+    Meteor.call('rollForPot', lottoId, pot, function(error, result){
+      if (error) {
+        return throwError(error.reason);
+      }
+      //console.log('result', result);
+    });
+  },
+  'click .unroll': function(e) {
+    e.preventDefault();
+    if (confirm("Are you sure you want to undo the roll?")) {
+      var lottoId = Session.get('lottoId');
+      var pot = this.toggle;
+      Meteor.call('unrollForPot', lottoId, pot, function(error, result){
+        if (error) {
+          return throwError(error.reason);
+        }
+      });
+    }
+  }
+});
